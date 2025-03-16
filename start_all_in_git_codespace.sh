d108456f7adea24ade5cdb5b43c78a61bbec8ad5
Hassen Amri <Hassenamri005@gmail.com>
2025-03-16 10:52:16 +0000

    feat: start project in github codespace ✅

---
+++ start_all_in_git_codespace.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+echo "................ [INFO] Preparing BACKEND infrastructure ................"
+cd backend/
+cat .env.example >> .env
+echo -e  >> .env
+npm i
+docker-compose up --build -d
+npm run swagger:ts
+echo "[INFO] Preparing Backend infrastructure done."
+
+echo "................ [INFO] Preparing FRONTEND infrastructure ................"
+cd ../frontend
+rm .env
+cat .env.example >> .env
+echo -e  >> .env
+echo -e VITE_BACKEND_API_URL=https://$(gh codespace view | awk '/Name/{print $2; exit}')-6001.app.github.dev >> .env
+cp -f ../backend/src/api/myApi.ts ./src/api/myApi.ts
+npm i
+docker-compose --profile dev up --build -d
+echo "[INFO] Preparing FRONTEND infrastructure done."
+
